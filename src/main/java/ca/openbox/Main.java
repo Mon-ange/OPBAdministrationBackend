54983fce8f92302a91797e76475df23f2611b63c
古城秋月 <acgucheng@gmail.com>
2024-03-26 19:33:05 -0700

    Use Jasypt to encrypt application.yml

--- src/main/java/ca/openbox/Main.java
+++ src/main/java/ca/openbox/Main.java
@@ -1,9 +1,11 @@
 package ca.openbox;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
