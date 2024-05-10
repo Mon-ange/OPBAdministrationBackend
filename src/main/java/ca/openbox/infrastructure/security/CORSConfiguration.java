1dbc76122f25dd7d58cc928b1467b09b9ca1e8b6
古城秋月 <acgucheng@gmail.com>
2024-05-09 17:10:46 -0700

    Changed database to AWS, add jwt secret key

---
+++ src/main/java/ca/openbox/infrastructure/security/CORSConfiguration.java
@@ -0,0 +1,19 @@
+package ca.openbox.infrastructure.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfiguration {
+    public WebMvcConfigurer corsConfig(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8081", "https://api-openbox.brimon.me/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
