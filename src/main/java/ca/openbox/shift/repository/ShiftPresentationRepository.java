23622f5efcf85980edb7628cf45ab7511e4d40fd
Monange <98monange@gmail.com>
2025-03-15 13:15:41 -0700

    Removed redundant queries, clarified methods, and added lunch break rule grouping

--- src/main/java/ca/openbox/shift/repository/ShiftPresentationRepository.java
+++ src/main/java/ca/openbox/shift/repository/ShiftPresentationRepository.java
@@ -10,33 +10,30 @@ import java.util.Date;
 import java.util.List;
 
 public interface ShiftPresentationRepository extends JpaRepository<ShiftPresentation, Integer> {
-    @Query( value = "select id, opb_user.username as username, start as date, name as user_real_name, start, end from opb_shift_arrangement\n"+
-            "left join opb_user on opb_shift_arrangement.username = opb_user.username\n" +
-            "where (start>=:start and start <=:end) \n" +
-            "and opb_shift_arrangement.status in (\"active\",\"cancelled\")",
-            nativeQuery = true)
-    public Collection<ShiftPresentation> getSchedulePresentationByStartdate(ZonedDateTime start, ZonedDateTime end);
 
-    @Query( value = "select id, opb_user.username as username, start as date, name as user_real_name, start, end from opb_shift_arrangement\n"+
+    @Query( value = "select id, opb_user.username as username, start as date, name as user_real_name, " +
+            "opb_shift_arrangement.group_name, start, end from opb_shift_arrangement\n"+
             "left join opb_user on opb_shift_arrangement.username = opb_user.username\n" +
             "where (start>=:start and start <=:end) \n" +
             "and opb_shift_arrangement.status in (\"active\",\"cancelled\") \n"+
             "and opb_shift_arrangement.username =:username",
             nativeQuery = true)
     public Collection<ShiftPresentation> getSchedulePresentationByUsernameAndStartdate(String username,ZonedDateTime start, ZonedDateTime end);
-    @Query( value = "select id, opb_user.username as username, start as date, name as user_real_name, start, end from opb_shift_arrangement\n"+
+
+    @Query( value = "select id, opb_user.username as username, start as date, name as user_real_name, " +
+            "opb_shift_arrangement.group_name, start, end from opb_shift_arrangement\n"+
             "left join opb_user on opb_shift_arrangement.username = opb_user.username\n" +
             "where (start>=:start and start <=:end) \n" +
-            "and opb_shift_arrangement.status in (\"active\",\"cancelled\")"+
-            "and :groupName=:groupName",
+            "and opb_shift_arrangement.status in (\"active\",\"cancelled\")",
             nativeQuery = true)
-    public List<ShiftPresentation> getByGroupAndTimeScope(String groupName, ZonedDateTime start, ZonedDateTime end);
+    public List<ShiftPresentation> getByTimeScope(ZonedDateTime start, ZonedDateTime end);
 
-    @Query( value = "select id, opb_user.username as username, start as date, name as user_real_name, start, end from opb_shift_arrangement\n"+
+    @Query( value = "select id, opb_user.username as username, start as date, name as user_real_name, " +
+            "opb_shift_arrangement.group_name, start, end from opb_shift_arrangement\n"+
             "left join opb_user on opb_shift_arrangement.username = opb_user.username\n" +
             "where (start>=:start and start <=:end) \n" +
             "and opb_shift_arrangement.status in (\"active\",\"cancelled\")"+
             "and opb_shift_arrangement.group_name=:groupName",
             nativeQuery = true)
-    public Collection<ShiftPresentation> getByGroupAndTimeScopeReal(String groupName, ZonedDateTime start, ZonedDateTime end);
+    public Collection<ShiftPresentation> getByGroupAndTimeScope(String groupName, ZonedDateTime start, ZonedDateTime end);
 }
